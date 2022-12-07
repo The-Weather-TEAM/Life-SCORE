7e07f78f8c0f63d472b7b02150de17bb06ff4729
Spaghet34 <101197359+Spaghet34@users.noreply.github.com>
2022-12-07 10:59:26 +0100

    Update fenetre.py

--- fenetre.py
+++ fenetre.py
@@ -1,1 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Code Tkinter ne pas oublier quand on crée des variable de les commenter (bon nom + fonction)
+
+modif précédente : 07/12/2022 10:50
+dernière modif : 07/12/2022 10:58
+"""
 from tkinter import *
+
+
+#fonctions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#fenêtre
+window = Tk() #fenetre de tkinter
+window.minsize(width=1020, height=768)
+window.resizable(False,False) #Taille non modifiable
+
+
+
+window.mainloop() #pour fermer la fenetre
