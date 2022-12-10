f7fb0d80b7a1f8b86b37d1b009155e678a3b8c2f
Spaghet34 <101197359+Spaghet34@users.noreply.github.com>
2022-12-10 19:11:20 +0100

    Update fenetre.py

--- code/fenetre.py
+++ code/fenetre.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
 """
 Code Tkinter ne pas oublier quand on crée des variable de les commenter (bon nom + fonction)
+réferences : 
+    - Les boutons s'écrivent btn_NOM
+    - Les fenêtres s'écrivent windowNOM
+    - Les textes (classe Message) s'écrivent msg_NOM
+    - d'autres à venir
 
 modif précédente : 08/12/2022 15:48
-dernière modif : 10/12/2022 19:00
+dernière modif : 10/12/2022 19:11
 """
 from tkinter import *
 from urllib.request import urlopen
 #from PIL import ImageTk, Image
 
-#fonctions
-"""fonction pour ouvrir la fenetre d'accueil"""
+global msg_principal #on pose les questions a travers lui
 
+
+
+#fonctions
+#premiere page
 def w_qcm(): #w pour window
+    global msg_principal
     """
     affiche la premiere page qui contient donc le qcm
     """
@@ -22,15 +31,39 @@ def w_qcm(): #w pour window
     #window.tk.call('tk::PlaceWindow', window)
     windowQCM.minsize(width=1020, height=768)
     windowQCM.resizable(False,False) #Taille non modifiable
-    #Plusieurs Bouttons 
+    #widgets
+    msg_principal =  Message(text="Bienvenue, nous allons commencer avec un petit test", width = 1000, font =('Bold',18), justify=CENTER) #font = taille + police justify comme sur word
+    msg_principal.place(relx= 0.5, rely=0.4, anchor = CENTER) #Anchor sert a le mettre au milieu et relx/rely le place a un % en x et en y 
+    #entree a utiliser peut etre ailleurs
+    """entree = Entry(windowQCM,width=30, font=('Bold',18))
+    entree.place(relx=0.5, rely= 0.45, anchor=CENTER) #Anchor sert a le mettre au milieu et relx/rely le place a un % en x et en y """
+    #bouton ok
+    btn_ok = Button(width=20, height=3, command=lambda: question1(windowQCM,btn_ok), bg='#70add7', text="OK") #appele la fonction question1
+    btn_ok.place(relx=0.5, rely=0.5,anchor=CENTER)
+    
     #affiche la photo
     #label = Label(windowQCM, image = photo)
     #label.place(x=10,y=0)
 
 
+
     windowQCM.mainloop() #pour fermer la fenetre
 
+def question1(fenetre,boutton):
+    global msg_principal
+    """
+    Passe à la question 1 et ouvre le qcm ajoute les deux boutons <-- et -->
+    """
+    boutton.destroy() #supprime ce bouton
+    btn_gauche = Button(width=20, height=3, command=lambda: print(), bg='#70add7', text="<---")
+    btn_droite = Button(width=20, height=3, command=lambda: print(), bg='#70add7', text="--->")
+    btn_gauche.place(relx=0.40,rely=0.5,anchor=CENTER)
+    btn_droite.place(relx=0.60,rely=0.5,anchor=CENTER)
+    msg_principal.config(text ="Vous êtes plutôt ?\nCalme                    Actif") #change le texte du msg principal
 
+
+
+#seconde page
 def w_question():
     """
     affiche la seconde page qui contient la requête de la ville
@@ -38,6 +71,7 @@ def w_question():
     pass
 
 
+#troisieme page
 def w_score():
     """
     affiche la dernière page qui contient le score et le bouton pour revenir
