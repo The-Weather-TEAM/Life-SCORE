2879aafd1551667696089c9d0ca6705128811c6a
Spaghet34 <101197359+Spaghet34@users.noreply.github.com>
2022-12-10 18:12:41 +0100

    Update fenetre.py

--- code/fenetre.py
+++ code/fenetre.py
@@ -2,8 +2,8 @@
 """
 Code Tkinter ne pas oublier quand on crée des variable de les commenter (bon nom + fonction)
 
-modif précédente : 07/12/2022 10:58
-dernière modif : 08/12/2022 15:48
+modif précédente : 08/12/2022 15:48
+dernière modif : 10/12/2022 19:00
 """
 from tkinter import *
 from urllib.request import urlopen
@@ -12,18 +12,34 @@ from PIL import ImageTk, Image
 #fonctions
 """fonction pour ouvrir la fenetre d'accueil"""
 
+def w_qcm(): #w pour window
+    """
+    affiche la premiere page qui contient donc le qcm
+    """
+    pass
 
 
+def w_question():
+    """
+    affiche la seconde page qui contient la requête de la ville
+    """
+    pass
 
 
+def w_score():
+    """
+    affiche la dernière page qui contient le score et le bouton pour revenir
+    """
+    pass
 
 
+"""
 #Image en url bitmap ?
 URL = "https://avatars.githubusercontent.com/u/119951824?s=200&v=4"
 u = urlopen(URL)
 raw_data = u.read()
 u.close()
-
+"""
 
 
 
