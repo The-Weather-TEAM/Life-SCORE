a7c8f1126cc743586cf6f4528f593232cc30b8e8
Nathan BOSY <nathan.bosy@gmail.com>
2023-01-14 23:00:25 +0100

    MAJ RECUP DATA (gestion semi-auto des csv)

--- code/recup_data_test.py
+++ code/recup_data_test.py
@@ -35,11 +35,19 @@ import random #Pour un easter egg
 
 
 
-def recup_donnees(infos_csv) :
+def recup_donnees(infos_csv, ville) :
     
-    lien_fichier = os.path.join(os.path.dirname(__file__), 'data')+'/'+infos_csv['nom_csv']+'.csv'
-    fichier = p.read_csv(lien_fichier, delimiter=infos_csv['delimiteur'],usecols=[infos_csv['colonne_ville'],infos_csv['colonne_donnee']],encoding='utf-8',low_memory=False)
-    rangee = fichier[fichier[infos_csv['colonne_ville']] == infos_csv['nom_csv']]
+    lien_fichier = os.path.join(os.path.dirname(__file__),'data')+'/'+infos_csv['nom_csv']+'.csv'
+    
+    fichier = p.read_csv(lien_fichier,
+                         delimiter=infos_csv['delimiteur'],
+                         usecols=[infos_csv['colonne_ville'],
+                                  infos_csv['colonne_donnee']],
+                         encoding='utf-8',
+                         low_memory=False)
+    
+    rangee = fichier[fichier[infos_csv['colonne_ville']] == ville]
+    print(rangee)
     
     try:
             return rangee.values[0][1]
@@ -72,4 +80,36 @@ potentiel_radon_avec_nom_ville = {'insee' : False,
                                   'nom_csv' : 'potentiel_radon'}
 
 
-print(recup_donnees(potentiel_radon))
+print(recup_donnees(potentiel_radon, '62820'))
+
+
+
+
+
+
+
+'''
+  DES FONCTIONS A
+UTILISER PLUS TARD
+
+(c'était pour tester chatgpt)
+
+'''
+
+
+
+
+def points_to_affine_function(x1, y1, x2, y2, x3, y3):
+    a = (y2 - y1) * (x3 - x2) - (y3 - y2) * (x2 - x1)
+    b = (x2 - x1) * (x3 - x2) + (y2 - y1) * (y3 - y2)
+    c = x2 * (x2 - x1) * y3 - x2 * (y2 - y1) * x3 + x1 * (y2 - y1) * x3 - y1 * (x3 - x2) * x2
+    return a,b,c
+
+
+def get_data_from_csv(file_name, data_to_find):
+    with open(file_name, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row['data_name'] == data_to_find:
+                return row['data_value']
+    return None
