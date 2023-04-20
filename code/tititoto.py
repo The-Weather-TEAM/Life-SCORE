959e9309ca315fa2574c88fcb66bd3c9196fc38f
Farenc Raphaël <101197359+Farenc21@users.noreply.github.com>
2023-04-20 14:45:01 +0200

    Create tititoto.py

---
+++ code/tititoto.py
@@ -0,0 +1,5 @@
+Raf
+fenetrePrincipale = interface.CTk() # fenetre de tkinter
+fenetrePrincipale.title('LifeScore  |  Menu principal')
+fenetrePrincipale.iconphoto(False, icone)
+fenetrePrincipale.minsize(width=1280  , height=848) # Taille minimum de la fenetre
