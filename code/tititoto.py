412f1c14acc083abc1cfc1c936efdc29d071125b
Frédo <114672957+fredericmarquet@users.noreply.github.com>
2023-04-20 14:46:08 +0200

    Update tititoto.py

--- code/tititoto.py
+++ code/tititoto.py
@@ -3,3 +3,19 @@ fenetrePrincipale = interface.CTk() # fenetre de tkinter
 fenetrePrincipale.title('LifeScore  |  Menu principal')
 fenetrePrincipale.iconphoto(False, icone)
 fenetrePrincipale.minsize(width=1280  , height=848) # Taille minimum de la fenetre
+
+
+
+Fred 
+"festivals":
+    ["62cf95993d99f22480f49334","47ac11c2-8a00-46a7-9fa8-9b802643f975", { "insee" : 1,
+                                                                          "delimiteur" : ";",
+                                                                          "colonne_ville" : 6,
+                                                                          "colonne_donnee" : [7],
+                                                                          "source" : "www.data.gouv.fr",
+                                                                          "type" : "oui_non",
+                                                                          "categorie": "qualité_de_vie",
+                                                                          "nom": "Les festivals"
+
+
+    }]
